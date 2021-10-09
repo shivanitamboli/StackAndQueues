6d7b4e13e8dce58bd69a463094cd6453ad3e6dbe
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-09 14:06:56 +0530

    Added : perform pop and peak method in stack

--- src/com/bridgelab/StackMain.java
+++ src/com/bridgelab/StackMain.java
@@ -8,6 +8,16 @@ public class StackMain {
 		stack.push(30);
 		stack.push(56);
 		stack.display();
+		// To perform peak and pop till the list is empty
+		stack.peak();
+		stack.pop();
+		stack.display();
+		stack.peak();
+		stack.pop();
+		stack.display();
+		stack.peak();
+		stack.pop();
+		stack.display();
 
 	}
 }
