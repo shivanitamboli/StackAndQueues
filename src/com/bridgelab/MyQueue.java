5a4ff92953fe465dabcb4fc8259eeae1e771ed3c
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-09 17:33:13 +0530

    Added : dequeue operations

--- src/com/bridgelab/MyQueue.java
+++ src/com/bridgelab/MyQueue.java
@@ -1,36 +1,48 @@
 package com.bridgelab;
+
 public class MyQueue {
-    MyNode front;
-    MyNode rear;
+	MyNode front;
+	MyNode rear;
 
-    /**
-     * This Method is used to add elements to Queue
-     *
-     * @param element - element to be added.
-     */
-    public void enqueue(int element) {
-        MyNode node = new MyNode();
-        node.key = element;
-        if (front == null) {
-            front = node;
-            rear = node;
-        } else {
-            rear.next = node;
-            rear = node;
-        }
-    }
+	/**
+	 * This Method is used to add elements to Queue
+	 *
+	 * @param element - element to be added.
+	 */
+	public void enqueue(int element) {
+		MyNode node = new MyNode();
+		node.key = element;
+		if (front == null) {
+			front = node;
+			rear = node;
+		} else {
+			rear.next = node;
+			rear = node;
+		}
+	}
 
-    /* This Method is used to display the elements in Queue. */
-    public void display() {
-        if (front == null)
-            System.out.println("Queue is empty");
-        else {
-            MyNode temp = front;
-            System.out.println("Queue list is -");
-            while (temp != null) {
-                System.out.println(temp.key);
-                temp = temp.next;
-            }
-        }
-    }
+	/* This method is used to delete elements from the queue */
+	public void dequeue() {
+		if (front == null)
+			System.out.println("Queue Underflow");
+		else {
+			MyNode temp = front;
+			System.out.println("Dequeued element is " + temp.key);
+			front = front.next;
+		}
+	}
+
+	/* This Method is used to display the elements in Queue. */
+	public void display() {
+		if (front == null)
+			System.out.println("Queue is empty");
+		else {
+			MyNode temp = front;
+			System.out.println("Queue list is -");
+			while (temp != null) {
+				System.out.println(temp.key);
+				temp = temp.next;
+			}
+		}
+	}
 }
--- src/com/bridgelab/QueueMain.java
+++ src/com/bridgelab/QueueMain.java
@@ -6,6 +6,7 @@ public class QueueMain {
 		queue.enqueue(56);
 		queue.enqueue(30);
 		queue.enqueue(70);
+		queue.dequeue();
 		queue.display();
 
 	}
